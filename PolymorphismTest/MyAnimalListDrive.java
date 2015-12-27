b85952fe7ea9601a3760b59b647b9f3320bb9571
zhangzhe <zhangzhe_millet@yeah.net>
2015-12-27 22:54:56 +0800

    add PolymorphismTest

---
+++ PolymorphismTest/MyAnimalListDrive.java
@@ -0,0 +1,12 @@
+public class MyAnimalListDrive {
+	
+	
+	public static void main(String[] args){
+		Cat c = new Cat();
+		Dog d = new Dog();
+		MyAnimalList myal = new MyAnimalList();
+	
+		myal.add(c);
+		myal.add(d);
+	}
+}
